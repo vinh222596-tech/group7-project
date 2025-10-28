811a238b926d39df439cebd8394714688741e628
quocvinh2 <vinh222596@student.nctu.edu.vn>
2025-10-28 13:42:15 +0700

    Add log from frontend

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ const PORT = process.env.PORT || 5000;
 // ⚡ Không được thiếu phần callback này nhé
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log("This is from frontend branch");
 });
-
